5807c3a204478fb5b04e5cf38206a876b1df66ae
Mahdi <44380968+mbonyani@users.noreply.github.com>
2025-10-04 14:19:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="rsl_rl",
+    version="1.0.2",
+    packages=find_packages(),
+    license="BSD-3",
+    description="Fast and simple RL algorithms implemented in pytorch",
+    python_requires=">=3.6",
+    install_requires=[
+        "GitPython", "gym", "numpy", "onnx", "tensorboard", "torch", "torchvision", "wandb",
+    ],
+)
