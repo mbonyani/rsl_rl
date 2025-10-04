fe6779bbc14a79d984fa3163476913e29238c590
Mahdi <44380968+mbonyani@users.noreply.github.com>
2025-10-04 15:04:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="rsl_rl",
-    version="1.0.2",
+    version="3.1.0",
     packages=find_packages(),
     license="BSD-3",
     description="Fast and simple RL algorithms implemented in pytorch",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         "GitPython", "gym", "numpy", "onnx", "tensorboard", "torch", "torchvision", "wandb",
     ],
